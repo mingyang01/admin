da5c8d0cae13aa4c059327537faa54b6a0319717
mingyang01 <mingyang@meilishuo.com>
2016-04-05 19:28:42 +0800

    test

--- Application/Home/Controller/UserController.class.php
+++ Application/Home/Controller/UserController.class.php
@@ -118,7 +118,7 @@ class UserController extends Controller {
     }
 
 
-
+    //test
     // 检测输入的验证码是否正确，$code为用户输入的验证码字符串
     function check_verify($code, $id = ''){
         $verify = new \Think\Verify();
